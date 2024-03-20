b09ddb13dde3ae735ff0f1cdce50e50648c54d26
cazzyjason <cazzyjason@gmail.com>
2024-03-21 02:28:31 +0800

    Add InventoryController and UIInventorypage

---
+++ Assets/Starter_Project_Inventory_System/Assets/_Scripts/InventoryController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+    [SerializeField]
+    private UIInventorypage inventoryUI;
+    //拿到背包UI
+
+    public int inventorySize = 10;
+
+    private void Start()
+    {
+        inventoryUI.InitializeInventoryUI(inventorySize);
+        //耦合 使用背包UI的方法，傳入size
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (inventoryUI.isActiveAndEnabled == false)
+            {
+                inventoryUI.Show();
+            }
+            else
+            {
+                inventoryUI.Hide();
+            }
+        }        
+    }
+}
---
+++ Assets/Starter_Project_Inventory_System/Assets/_Scripts/UI/UIInventorypage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInventorypage : MonoBehaviour
+{
+    [SerializeField]
+    private UIInventoryItem itemPrefab;
+
+    [SerializeField]
+    private RectTransform contentPanel;
+
+    List<UIInventoryItem> listOfUIItems = new List<UIInventoryItem>();
+
+
+
+
+    //以下為方法部分
+
+    //生成UI部分，邏輯:調用此方法，需傳入背包格數大小，接者會生成對應數量的背包UI
+    public void InitializeInventoryUI(int inventorysize)
+    {
+        for (int i = 0; i < inventorysize; i++)
+        {
+            UIInventoryItem uiItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
+            uiItem.transform.SetParent(contentPanel);
+            listOfUIItems.Add(uiItem);
+        }
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
